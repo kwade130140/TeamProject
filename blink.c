d9c18395443b078200a8554d69be36820684f539
Kyle <kbuilder1@gmail.com>
2018-04-19 18:11:44 -0500

    Added rotating camera stream

--- blink.c
+++ blink.c
@@ -37,36 +37,28 @@ int main(void) {
 			system("sudo uv4l restart");
 			printf("Starting stream... \n");
 			system("sudo uv4l --driver raspicam --width 640 --height 480 --framerate 5 --server-option '--port=8080'");
-			delay(1000);
-			printf("Opening stream \n");
-			system("sensible-browser http://localhost:8080/stream");
+			delay (1000);
 			inactive = 0;
 		}
 		else if(read2 == 1) {
 			system("sudo uv4l restart");
 			printf("Starting stream... \n");
 			system("sudo uv4l --driver uvc --device-path 001:006");
-			delay(1000);
-			printf("Opening stream \n");
-			system("sensible-browser http://localhost:8080/stream");
+			delay (1000);
 			inactive = 0;
 		}
 		else if(read3 == 1) {
 			system("sudo uv4l restart");
 			printf("Starting stream... \n");
-			system("sudo uv4l --driver uvc --device-path 001:005");
-			delay(1000);
-			printf("Opening stream \n");
-			system("sensible-browser http://localhost:8080/stream");
+			system("sudo uv4l --driver uvc --device-path 001:006");
+			delay (1000);
 			inactive = 0;
 		}
 		else if(read4 == 1) {
 			system("sudo uv4l restart");
 			printf("Starting stream... \n");
-			system("sudo uv4l --driver uvc --device-path 001:004");
-			delay(1000);
-			printf("Opening stream \n");
-			system("sensible-browser http://localhost:8080/stream");
+			system("sudo uv4l --driver uvc --device-path 001:006");
+			delay (1000);
 			inactive = 0;
 		}
 
@@ -74,28 +66,30 @@ int main(void) {
 		printf("Read1: %d, Read2: %d, Read3: %d, Read4: %d\n", read1, read2, read3, read4);
 	}
 
-	while(inactive == 1) {
+	int streamNo = 0;
+	while(inactive == 0) {
+		delay(10000);
+		system("sudo uv4l restart");
+		printf("Switching stream... \n");
 
-		int read1 = digitalRead(PIR1);
-		int read2 = digitalRead(PIR2);
-		int read3 = digitalRead(PIR3);
-		int read4 = digitalRead(PIR4);
-
-		if(read1 == 1) {
-
+		switch(streamNo) {
+		case 0:
+			system("sudo uv4l --driver uvc --device-path 001:006");
+			streamNo = 1;
+			break;
+		case 1:
+			system("sudo uv4l --driver uvc --device-path 001:006");
+			streamNo = 2;
+			break;
+		case 2:
+			system("sudo uv4l --driver uvc --device-path 001:006");
+			streamNo = 3;
+			break;
+		case 3:
+			system("sudo uv4l --driver raspicam --width 640 --height 480 --framerate 10 --server-option '--port=8080'");
+			streamNo = 0;
+			break;
 		}
-		else if(read2 == 1) {
-
-		}
-		else if(read3 == 1) {
-
-		}
-		else if(read4 == 1) {
-
-		}
-
-		delay (500);
-		printf("Streaming - Read1: %d, Read2: %d, Read3: %d, Read4: %d\n", read1, read2, read3, read4);
 	}
 
 	printf("Exiting... \n");
